b4bda400f11f33d14173b2bc9a6d240a77119aab
Ruben Aguilera <radhcode@gmail.com>
2021-06-28 02:14:27 +0200

    chore: initial commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+interface Todo {
+    title: string;
+    description: string;
+}
+
+const todo1: Todo = {
+    title: 'organize desk',
+    description: 'clear clutter'
+};
+
+console.log(todo1);
---
+++ src/calc.ts
@@ -0,0 +1,6 @@
+
+export class Calc {
+    add (a: number, b: number): number {
+        return a + b;
+    }
+}
